62f51715299ddedbd068a183c5e8d6e3a3766d93
Loyiso <Loyiso@projectcodex.co>
2017-05-10 15:18:54 +0200

    first commit

---
+++ Avo.js
@@ -0,0 +1,14 @@
+function avoDeal(avoStock) {
+
+  for (var i = 0; i < avoStock.length; i++) {
+    var newStock = avoStock.split(",");
+    var newArray = newStock.trim
+
+  return newArray
+
+  }
+
+}
+
+console.log(avoDeal("1 for R3, 2 for R7, 3 for R10, 5 for R14.50"
+));
